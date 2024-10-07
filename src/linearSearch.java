792b35f6fd8b9a50da3aa846a46ad6222aeefad7
Rushi Pisole <rushikeshpisole3@gmail.com>
2024-10-07 11:10:45 +0530

    Linear Search By String

--- src/linearSearch.java
+++ src/linearSearch.java
@@ -1,9 +1,10 @@
 public class linearSearch {
     public static void main(String[] args) {
-        int number[] = {2, 3, 4, 5, 6, 7, 8, 9};
-        int key = 10;
+//        int number[] = {2, 3, 4, 5, 6, 7, 8, 9};
+        String menu[] = {"dosa", "pohe", "idli", "upma"};
+        String key = "dosa";
 
-        int index = linearSearch(number, key);
+        int index = linearSearch(menu, key);
         if (index == -1) {
             System.out.println("Not found");
         } else {
@@ -11,7 +12,7 @@ public class linearSearch {
         }
     }
 
-    public static int linearSearch(int number[], int key) {
+    public static int linearSearch(String number[], String key) {
         for (int i = 0; i < number.length; i++) {
             if (number[i] == key) {
                 return i;
