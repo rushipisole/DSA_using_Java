d21f95477abf32927cd52c1861d5cc8f111619f7
Rushi Pisole <rushikeshpisole3@gmail.com>
2024-10-08 10:18:41 +0530

    Reverse the Array

---
+++ src/reverseAnArray.java
@@ -0,0 +1,26 @@
+public class reverseAnArray {
+    public static void main(String[] args) {
+        int number[] = {2, 3, 4, 5, 6, 7, 8, 9};
+        reverseArray(number);
+
+        for (int i = 0; i < number.length; i++) {
+            System.out.print(number[i] + " ");
+        }
+        System.out.println();
+
+    }
+
+    public static void reverseArray(int number[]) {
+        int first = 0;
+        int last = number.length - 1;
+
+        while (first < last) {
+            int temp = number[first];
+            number[first] = number[last];
+            number[last] = temp;
+
+            first++;
+            last--;
+        }
+    }
+}
