ffac18037d0608e413707c70d5231cd2437592d1
Rushi Pisole <rushikeshpisole3@gmail.com>
2024-10-09 11:21:52 +0530

    Print the all sub arrays

---
+++ src/printSubarrays.java
@@ -0,0 +1,22 @@
+public class printSubarrays {
+    public static void main(String[] args) {
+        int[] number = {2, 4, 6, 8, 10};
+        printSubArrays(number);
+    }
+
+    public static void printSubArrays(int number[]) {
+        for (int i = 0; i < number.length; i++) {
+            int start = i;
+
+            for (int j = i; j < number.length; j++) {
+                int end = j;
+
+                for (int k = start; k <= end; k++) {
+                    System.out.print(number[k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}
