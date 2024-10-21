9728154039338c552ad2c786f66653d6e5dbf1d8
Rushi Pisole <rushikeshpisole3@gmail.com>
2024-10-21 23:24:46 +0530

    Bubble Sort

---
+++ src/bubbleSort.java
@@ -0,0 +1,28 @@
+public class bubbleSort {
+    private static int turn;
+
+    public static void main(String[] args) {
+        int arr[] = {2, 4, 1, 6, 5, 3};
+        bubbleSort(arr);
+        bubbleSortPrint(arr);
+    }
+
+    public static void bubbleSortPrint(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void bubbleSort(int arr[]) {
+        for (int turn = 0; turn < arr.length - 1; turn++) {
+            for (int j = 0; j < arr.length - 1 - turn; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
