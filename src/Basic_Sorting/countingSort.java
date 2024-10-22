61f5ada0fab88eb6f512c038d3ff20b19ff7e6da
Rushi Pisole <rushikeshpisole3@gmail.com>
2024-10-22 23:47:13 +0530

    Counting Sort

---
+++ src/Basic_Sorting/countingSort.java
@@ -0,0 +1,35 @@
+package Basic_Sorting;
+
+public class countingSort {
+    public static void main(String[] args) {
+        int arr[] = {2, 7, 5, 3, 4, 62, 1, 2, 3};
+        countingSort(arr);
+        countingSortPrint(arr);
+    }
+
+    public static void countingSortPrint(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void countingSort(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+        int count[] = new int[largest + 1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+}
