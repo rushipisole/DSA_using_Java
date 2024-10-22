7e496b634fdacce68daab1a48bdbc2cb8097d0cc
Rushi Pisole <rushikeshpisole3@gmail.com>
2024-10-22 22:59:38 +0530

    Insetation Sort

---
+++ src/insertationSort.java
@@ -0,0 +1,30 @@
+public class insertationSort {
+    public static void main(String[] args) {
+        int arr[] = {6, 2, 4, 5, 1, 9};
+        insertationSort(arr);
+        insertationSortPrint(arr);
+    }
+
+    public static void insertationSortPrint(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void insertationSort(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+
+            //Finf out the correct position to insert
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+
+            //Insertion
+            arr[prev + 1] = curr;
+        }
+    }
+}
